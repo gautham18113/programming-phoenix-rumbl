49723e68cf9eb9e41247e57fca997f5421b3d8a3
Gautham <gautham18113@gmail.com>
2020-10-11 10:59:03 -0700

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :rumbl,
+  ecto_repos: [Rumbl.Repo]
+
+# Configures the endpoint
+config :rumbl, RumblWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "5/pE7UVHpgMSGgJ753rFIkBU479p7j4oa9Xw8HrjkAHmWSuRIgrSsDtIgEDS7z0Z",
+  render_errors: [view: RumblWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Rumbl.PubSub,
+  live_view: [signing_salt: "o1+KG8/l"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
